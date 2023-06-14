b0ea4d1a87a67b3f04643e03386c25d884fb23df
David <david.ndiba@student.moringaschool.com>
2023-06-14 15:07:56 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+
+  let travel = city === "NYC" ? "Ok, sounds good." : "No go."
+  return travel;
+}
+function switchOnCharmFromTip(tip) {
+  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      
+      case "not as generous":
+        return "Thank you.";
+        
+        default:
+          return `Bye.`;
+          
+          
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
